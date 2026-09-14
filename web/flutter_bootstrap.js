156ea2b105b8e6837392cf04bab9e2b47dcabeab
malazhub <malazjanbeih@gmail.com>
2026-09-14 23:06:32 +0300

    Force CPU CanvasKit on Android web

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,12 @@
+﻿{{flutter_js}}
+{{flutter_build_config}}
+
+const isAndroid = /Android/i.test(navigator.userAgent);
+
+_flutter.loader.load({
+  config: isAndroid
+      ? {
+          canvasKitForceCpuOnly: true,
+        }
+      : {},
+});
