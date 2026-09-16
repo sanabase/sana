7c8889c0563b34a2f7b6ff664eb1e8c85c5e6d92
malazhub <malazjanbeih@gmail.com>
2026-09-16 20:30:30 +0300

    Add non-web stub for SanaWebPush with conditional import

---
+++ lib/sana_web_push_stub.dart
@@ -0,0 +1,13 @@
+﻿// lib/sana_web_push_stub.dart
+// Non-web stub for SanaWebPush.
+// Used automatically on Android, iOS, Windows, macOS, Linux.
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SanaWebPush {
+  static Future<bool> isSubscribed() async => false;
+  static Future<bool> enable(SupabaseClient client) async => false;
+  static Future<String> enableVerbose(SupabaseClient client) async => 'NOT_WEB';
+  static Future<void> disable(SupabaseClient client) async {}
+  static String? browserTimeZone() => null;
+}
