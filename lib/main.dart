e367fe8847a442b2bea3c89cbb1cb0456419ecd9
malazhub <malazjanbeih@gmail.com>
2026-09-17 08:11:42 +0300

    Gate reminder alarm scheduling on reminders_enabled

--- lib/main.dart
+++ lib/main.dart
@@ -5359,7 +5359,8 @@ class _RecordListScreenState extends State<RecordListScreen> {
           _loading = false;
         });
 
-        if (_table == 'reminders') {
+if (_table == 'reminders' &&
+    _profile?['reminders_enabled'] != false) {
           for (final row in records) {
             try {
               await SanaAlarmService.scheduleReminder(row);
@@ -5633,7 +5634,8 @@ class _RecordListScreenState extends State<RecordListScreen> {
     try {
       print('Inserting into $_table: $cleanPayload');
 
-      if (_table == 'reminders') {
+if (_table == 'reminders' &&
+    _profile?['reminders_enabled'] != false) {
         final inserted =
             await _client.from(_table).insert(cleanPayload).select().single();
 
