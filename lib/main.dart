89cdcdfe780c7e463c032122473a0bd977ddfceb
malazhub <malazjanbeih@gmail.com>
2026-09-17 12:58:48 +0300

    Guard popUntil against empty navigator stack in alarm screen

--- lib/main.dart
+++ lib/main.dart
@@ -6973,12 +6973,15 @@ class _SanaAlarmScreenState extends State<SanaAlarmScreen> {
     if (!mounted) return;
 
     // Return completely to the application's first/home route.
-    Navigator.of(
+    final nav = Navigator.of(
       context,
       rootNavigator: true,
-    ).popUntil(
-      (route) => route.isFirst,
     );
+    if (nav.canPop()) {
+      nav.popUntil(
+        (route) => route.isFirst,
+      );
+    }
   }
 
   Future<void> _closeAlarmScreen() async {
@@ -6989,12 +6992,15 @@ class _SanaAlarmScreenState extends State<SanaAlarmScreen> {
 
     if (!mounted) return;
 
-    Navigator.of(
+    final nav = Navigator.of(
       context,
       rootNavigator: true,
-    ).popUntil(
-      (route) => route.isFirst,
     );
+    if (nav.canPop()) {
+      nav.popUntil(
+        (route) => route.isFirst,
+      );
+    }
   }
 
   @override
