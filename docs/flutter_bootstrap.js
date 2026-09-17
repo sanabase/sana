3d7fc95adb5a5d903d46312380a408bf90e5d0d3
malazhub <malazjanbeih@gmail.com>
2026-09-17 21:37:35 +0300

    Deploy GPU rendering fix

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -36,12 +36,4 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c0010","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 
-const isAndroid = /Android/i.test(navigator.userAgent);
-
-_flutter.loader.load({
-  config: isAndroid
-      ? {
-          canvasKitForceCpuOnly: true,
-        }
-      : {},
-});
+_flutter.loader.load();
