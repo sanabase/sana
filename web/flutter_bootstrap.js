393d60ea12e620318076645738f14e32b418ebea
malazhub <malazjanbeih@gmail.com>
2026-09-17 21:36:00 +0300

    Remove canvasKitForceCpuOnly flag

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,12 +1,4 @@
-﻿{{flutter_js}}
+{{flutter_js}}
 {{flutter_build_config}}
 
-const isAndroid = /Android/i.test(navigator.userAgent);
-
-_flutter.loader.load({
-  config: isAndroid
-      ? {
-          canvasKitForceCpuOnly: true,
-        }
-      : {},
-});
+_flutter.loader.load();
