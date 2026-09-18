dd6bc0da5bef6f4bd2f6d94a34b93e69c39da21f
malazhub <malazjanbeih@gmail.com>
2026-09-18 10:02:33 +0300

    Close card tries window.close then history.back

--- lib/sana_web_push_web.dart
+++ lib/sana_web_push_web.dart
@@ -110,6 +110,12 @@ class SanaWebPush {
   }
   static void closeApp() {
     try { web.window.close(); } catch (_) {}
+    try {
+      if (web.window.history.length > 1) {
+        web.window.history.back();
+        return;
+      }
+    } catch (_) {}
     try { web.window.location.href = 'about:blank'; } catch (_) {}
   }
 
