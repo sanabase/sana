1407f81c92b87d95e957c61b14e9725d411014d1
malazhub <malazjanbeih@gmail.com>
2026-09-18 09:51:00 +0300

    Deploy close navigation

--- docs/main.dart.js
+++ docs/main.dart.js
@@ -33694,7 +33694,7 @@ case 6:case 1:return A.p(q,r)
 case 2:return A.o(o.at(-1),r)}})
 return A.q($async$tF,r)},
 bbZ(){var s
-try{v.G.window.close()}catch(s){}},
+try{v.G.window.close()}catch(s){}try{v.G.window.location.href="about:blank"}catch(s){}},
 bbY(){var s,r,q,p,o,n,m,l=null
 try{s=v.G.Intl
 if(s==null)return l
