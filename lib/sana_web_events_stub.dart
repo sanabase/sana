2e561a0fcc17a97f1ea93f8fe6190acf80b81d4c
malazhub <malazjanbeih@gmail.com>
2026-09-23 20:56:15 +0300

    Add web event listener files

---
+++ lib/sana_web_events_stub.dart
@@ -0,0 +1,9 @@
+﻿class SanaWebEvents {
+  static void installPopStateListener(
+    void Function(String?) onReminder,
+  ) {}
+
+  static void installServiceWorkerMessageListener(
+    void Function(Map<dynamic, dynamic>) onMessage,
+  ) {}
+}
---
+++ lib/sana_web_events_web.dart
@@ -0,0 +1,38 @@
+﻿import 'dart:js_interop';
+import 'package:web/web.dart' as web;
+
+class SanaWebEvents {
+  static void installPopStateListener(
+    void Function(String?) onReminder,
+  ) {
+    try {
+      web.window.addEventListener(
+        'popstate',
+        (web.Event _) {
+          onReminder(Uri.base.queryParameters['reminder']);
+        }.toJS,
+      );
+    } catch (_) {}
+  }
+
+  static void installServiceWorkerMessageListener(
+    void Function(Map<dynamic, dynamic>) onMessage,
+  ) {
+    try {
+      web.window.navigator.serviceWorker.addEventListener(
+        'message',
+        (web.Event event) {
+          try {
+            final msg = event as web.MessageEvent;
+            final data = msg.data;
+            if (data == null) return;
+            final map = data.dartify();
+            if (map is Map) {
+              onMessage(map);
+            }
+          } catch (_) {}
+        }.toJS,
+      );
+    } catch (_) {}
+  }
+}
